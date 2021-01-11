fe39e5394d1ad56e416e611c8ed986edc0eee0a7
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-01-10 18:10:53 -0700

    updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-// Commas are important to separate the elements
+// Commas are important to separate the elements in the array
 var questionArray = [
   {
     question: "How many people are on Team Avatar?",
@@ -44,45 +43,41 @@ var questionArray = [
 ];
 
 // Declaring variables globally
-
 var questionNumber = -1;
 var countdown = 60;
-
 var questionDiv = document.querySelector("#questionDiv")
 var choicesDiv = document.querySelector("#choicesDiv")
-var messageElem = document.querySelector("#messageElem")
+var messageElement = document.querySelector("#messageElement")
 var nextQuestionBtn = document.querySelector("#nextQuestionBtn")
+var quizStart = false;
 
-var quizStarted = false;
-
+// Timer function - Makes the coundown start
 var countdown = setInterval(function(){
-  if(quizStarted){
-  document.querySelector("#timeLet").textContent = time
+  if(quizStart){
+  document.querySelector("#timeLeft").textContent = time
   time --
-  }
-  if (time === 0){
+  } 
+  if(time === 0){
     endQuiz ();
   }
 }, 1000);
 
+// Function that gets everything started
 var startFunction = function(){
-  // Hiding the start button and H1
   document.querySelector("#header1").style.display = "none"
-  var startBtn = document.querySelector("#startBtn")
-  startBtn.style.visibility = "hidden";
-  // Could be written as document.querySelector("#startBtn").style.visibility = "hidden"
-
-  // Display questions one at a time. Run the nextQuestion function
+  var startQuiz = document.querySelector("#startQuiz")
+  startQuiz.style.visibility = "hidden";
+  // Could be written as document.querySelector("#startQuiz").style.visibility = "hidden"
   nextQuestion()
-
-  quizStarted = true;
+  quizStart = true;
 };
 
-document.querySelector("#startBtn").addEventListener("click", startFunction)
+document.querySelector("#startQuiz").addEventListener("click", startFunction)
 
+// Move on to the next question
 var nextQuestion = function(){
   questionNumber ++;
-  messageElem.textContent = "";
+  messageElement.textContent = "";
   nextQuestionBtn.style.visibility = "hidden";
 
   /* Grab a question from the questionArray
@@ -90,7 +85,6 @@ var nextQuestion = function(){
   Calling out the first question by adding .question */
   var questionString = questionArray[questionNumber].question
   
-  // H3
   var newQuestion = document.createElement("h3")
   newQuestion.textContent = questionString;
   
@@ -103,22 +97,22 @@ var nextQuestion = function(){
   var D = questionArray[questionNumber].D;
 
   var buttonA = document.createElement("button")
-  buttonA.textContent = choiceA;
+  buttonA.textContent = A;
   buttonA.addEventListener("click", submitAnswer)
   choicesDiv.appendChild(buttonA)
 
   var buttonB = document.createElement("button")
-  buttonB.textContent = choiceB;
+  buttonB.textContent = B;
   buttonB.addEventListener("click", submitAnswer)
   choicesDiv.appendChild(buttonB)
 
   var buttonC = document.createElement("button")
-  buttonC.textContent = choiceC;
+  buttonC.textContent = C;
   buttonC.addEventListener("click", submitAnswer)
   choicesDiv.appendChild(buttonC)
 
   var buttonD = document.createElement("button")
-  buttonD.textContent = choiceD;
+  buttonD.textContent = D;
   buttonD.addEventListener("click", submitAnswer)
   choicesDiv.appendChild(buttonD)
 };
@@ -126,9 +120,9 @@ var nextQuestion = function(){
 var submitAnswer = function(answerChoice){
   if(answerChoice === questionArray[questionNumber].correctAnswer){ // Correct answer
   // Tildes (`) are great and can use the template literal
-    messageElem.textContent = `${answerChoice} is correct!`
+    messageElement.textContent = `${answerChoice} is correct!`
   } else {
-    messageElem.textContent = `${answerChoice} is wrong!`
+    messageElement.textContent = `${answerChoice} is wrong!`
   }
 
   // Clear out question and choices
